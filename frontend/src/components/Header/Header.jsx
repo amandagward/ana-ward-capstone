92046a22a9c85668e9a0d053f726acf0d8075d61
Ana Ward <amandagward@me.com>
2025-02-22 23:45:19 -0500

    styling updates to frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import "./Header.scss";
 
 export default function Header() {
     return (
-        <div className="Header">
-            Header
+        <div className="header">
+            <h1>Tooth Social</h1>
         </div>
     )
 }
